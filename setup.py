b3f8733e875f3f64e60716c461cdee70ed189e0d
paulwinex <tekken3>
2018-03-09 10:56:19 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='py_cerebro',
+      version='',
+      description='',
+      author='Cinesoft',
+      author_email='',
+      packages=[],
+      scripts=[]
+      )
+
